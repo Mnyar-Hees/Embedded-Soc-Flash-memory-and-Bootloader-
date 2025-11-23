05f92e6b610a35c76b8af1a7f13c7532bc5ac5b1
Mnyar Hees <menyar.hees@gmail.com>
2025-11-23 14:23:10 +0100

    Add files via upload

---
+++ Software/VGA_IP_SW.tcl
@@ -0,0 +1,12 @@
+create_driver vga_ip
+set_sw_property hw_class_name vga_ip
+set_sw_property version 1
+set_sw_property min_compatible_hw_version 1.0
+add_sw_property bsp_subdirectory drivers
+add_sw_property c_source HAL/src/char_map.c
+add_sw_property c_source HAL/src/draw_vga.c
+add_sw_property include_source HAL/inc/char_map.h
+add_sw_property include_source HAL/inc/draw_vga.h
+add_sw_property include_source HAL/inc/DE10_Lite_VGA_Driver.h
+add_sw_property supported_bsp_type HAL
+
